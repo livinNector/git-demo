d1938e72c06a9f278e2136a351a9d278b1321840
manojpod <manojkumar@study.iitm.ac.in>
2025-01-30 17:41:48 +0530

    added git commands

--- hello-world.py
+++ hello-world.py
@@ -7,3 +7,23 @@ import numpy as np
 a = 3
 b = 4
 print(a/b)
+
+git remote get -url origin
+
+git checkout -b hello-world
+
+git add hello-world.py 
+git commit --amend
+
+# remove all the updates and go back to last commited work
+git checkout hello-world.py 
+
+git stash -m "write some comment"
+
+git stash pro
+
+
+
+$ git push --set-upstream origin hello-world
+
+$ git push --set-upstream origin hello-world
